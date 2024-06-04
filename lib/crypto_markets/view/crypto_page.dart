758053e62750b490ba46af5ef18e62b217a64948
Kevin Kamto <kamtokevin@gmail.com>
2024-06-04 16:26:37 -0400

    feat(WIP): basic cryto page

---
+++ lib/crypto_markets/view/crypto_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CryptoPage extends StatelessWidget {
+  const CryptoPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // return BlocProvider(create: (context) => CryptoPageBloc(
+
+    // ));
+    return Column();
+  }
+}
